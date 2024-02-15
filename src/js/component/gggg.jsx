0769984629a27f78eb36c02c2e237e433a0891e5
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-02-15 01:12:28 +0000

    trafficlight1

---
+++ src/js/component/gggg.jsx
@@ -0,0 +1,12 @@
+<div className="semaforo">
+            
+<div className="base">
+    <div className="Lights">
+
+        <div className="light"></div>
+        <div className="light"></div>
+        <div className="light"></div>
+
+    </div>
+</div>
+</div>
